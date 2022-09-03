a78a44dbc7b6fe0803c08d21f76e6e4ccdd6b8fe
Sandeep Kandula <sandeep.kandula@thermofisher.com>
2022-09-03 20:19:00 +0530

    Forms. Binding and Validation.

---
+++ 05/WiredBrainCoffeeAdmin/Data/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WiredBrainCoffeeAdmin.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [MinLength(20, ErrorMessage = "The description should be at least 20 characters with meaningful content")]
+        [Required]
+        public string Description { get; set; }
+
+        [MaxLength(30)]
+        [Required]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        public IFormFile Upload { get; set; }
+    }
+}
